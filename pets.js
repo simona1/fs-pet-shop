69cec8673ea3fa6a65a34ff74dee446b82b735a1
Stela Palii <scientia.ssp@gmail.com>
2017-06-02 13:46:13 -0700

    Refactored Part 1 code

--- pets.js
+++ pets.js
@@ -3,106 +3,135 @@
 'use strict';
 
 const fs = require('fs');
-
 const path = require('path');
+
+const CMDS = {
+  read: readPromise,
+  create: createPromise,
+  update: updatePromise,
+  destroy: destroyPromise,
+};
+
 const petsPath = path.join(__dirname, 'pets.json');
 
-const node = path.basename(process.argv[0]);
-const file = path.basename(process.argv[1]);
-const cmd = process.argv[2];
-const option = process.argv[3];
-let args = process.argv;
+const ARGS = process.argv.slice(0);
+const NODE = path.basename(ARGS.shift());
+const FILE = path.basename(ARGS.shift());
 
-if (cmd === 'read') {
-  fs.readFile(petsPath, 'utf8', function(err, data) {
-    if (err) {
-      throw err;
-    }
-    const pets = JSON.parse(data);
-    const index = parseInt(option);
-    if (isNaN(index)) {
-      console.log(pets);
-    } else if (index < 0 || index >= pets.length) {
-      console.error(`Usage: ${node} ${file} read INDEX`);
-      process.exit(1);
-    } else {
-      console.log(pets[index]);
-    }
+function main() {
+  const cmd = ARGS.shift();
+  if (!CMDS.hasOwnProperty(cmd)) {
+    console.error(`Usage: ${NODE} ${FILE} [${Object.keys(CMDS).join(' | ')}]`);
+    process.exit(1);
+  }
+
+  CMDS[cmd].apply(null, ARGS).catch(err => {
+    console.error(err.message);
+    process.exit(1);
   });
-} else if (cmd === 'create') {
-  fs.readFile(petsPath, 'utf8', function(readErr, data) {
-    if (readErr) {
-      throw readErr;
-    } else if (args.length <= 5) {
-      console.error(`Usage: ${node} ${file} create AGE KIND NAME`);
-      process.exit(1);
-    }
-    const pets = JSON.parse(data);
-    const pet = {};
-    const age = parseInt(args[3]);
-    pet.age = age;
-    pet.kind = args[4];
-    pet.name = args[5];
+}
 
-    pets.push(pet);
-    const petsJSON = JSON.stringify(pets);
-
-    fs.writeFile(petsPath, petsJSON, function(writeErr) {
-      if (writeErr) {
-        throw writeErr;
+function readFilePromise(path) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(path, 'utf8', function(err, data) {
+      if (err) {
+        reject(err);
+        return;
       }
+      resolve(JSON.parse(data));
+    });
+  });
+}
+
+function writeFilePromise(path, data) {
+  return new Promise((resolve, reject) => {
+    fs.writeFile(path, JSON.stringify(data), function(writeErr) {
+      writeErr ? reject(writeErr) : resolve();
+    });
+  });
+}
+
+function readPromise(index) {
+  return readFilePromise(petsPath).then(pets => {
+    if (index == null) {
+      console.log(pets);
+      return pets;
+    }
+    const pet = pets[parseInt(index)];
+    if (pet) {
+      console.log(pet);
+      return pet;
+    }
+    throw new Error(`Usage: ${NODE} ${FILE} read INDEX`);
+  });
+}
+
+function createPromise(age, kind, name) {
+  if (age == null || kind == null || name == null) {
+    return Promise.reject(
+      new Error(`Usage: ${NODE} ${FILE} create AGE KIND NAME`)
+    );
+  }
+  return readFilePromise(petsPath).then(pets => {
+    const pet = {
+      age: parseInt(age),
+      kind: kind,
+      name: name,
+    };
+    pets.push(pet);
+
+    return writeFilePromise(petsPath, pets).then(() => {
       console.log(pet);
     });
   });
-} else if (cmd === 'update') {
-  fs.readFile(petsPath, 'utf8', function(readErr, data) {
-    if (readErr) {
-      throw readErr;
-    } else if (args.length <= 6) {
-      console.error(`Usage: ${node} ${file} update INDEX AGE KIND NAME`);
-      process.exit(1);
-    }
-    const pets = JSON.parse(data);
-    const index = parseInt(args[3]);
-    const age = parseInt(args[4]);
-    const kind = args[5];
-    const name = args[6];
-    const newPet = pets[index];
-    newPet.age = age;
-    newPet.kind = kind;
-    newPet.name = name;
-    pets[index] = newPet;
-    const petsJSON = JSON.stringify(pets);
-
-    fs.writeFile(petsPath, petsJSON, function(writeErr) {
-      if (writeErr) {
-        throw writeErr;
-      }
-      console.log(newPet);
-    });
-  });
-} else if (cmd === 'destroy') {
-  fs.readFile(petsPath, 'utf8', function(err, data) {
-    if (err) {
-      throw err;
-    }
-    if (args.length === 3) {
-      console.error(`Usage: ${node} ${file} destroy INDEX`);
-      process.exit(1);
-    }
-    const pets = JSON.parse(data);
-    const index = parseInt(args[3]);
-    const res = pets.splice(index, 1)[0];
-
-    const petsJSON = JSON.stringify(pets);
-    fs.writeFile(petsPath, petsJSON, function(writeErr) {
-      if (writeErr) {
-        throw writeErr;
-      }
-      console.log(res);
-    });
-  });
-} else {
-  console.error(`Usage: ${node} ${file} [read | create | update | destroy]`);
-  process.exit(1);
 }
+
+function updatePromise(index, age, kind, name) {
+  const usage = `Usage: ${NODE} ${FILE} update INDEX AGE KIND NAME`;
+
+  if (index == null || age == null || kind == null || name == null) {
+    return Promise.reject(new Error(usage));
+  }
+
+  return readFilePromise(petsPath).then(
+    pets => {
+      const pet = pets[parseInt(index)];
+      if (!pet) {
+        throw new Error(usage);
+      }
+
+      pet.age = parseInt(age);
+      pet.kind = kind;
+      pet.name = name;
+
+      return writeFilePromise(petsPath, pets).then(() => {
+        console.log(pet);
+      });
+    }
+  );
+}
+
+function destroyPromise(index) {
+  const usage = `Usage: ${NODE} ${FILE} destroy INDEX`;
+
+  if (index == null) {
+    return Promise.reject(new Error(usage));
+  }
+
+  return readFilePromise(petsPath).then(
+    pets => {
+      index = parseInt(index);
+      const pet = pets[index];
+      if (!pet) {
+        throw new Error(usage);
+      }
+      pets = pets.filter((pet, i) => i !== index);
+
+      return writeFilePromise(petsPath, pets).then(() => {
+        console.log(pet);
+      });
+    }
+  );
+}
+
+main();
